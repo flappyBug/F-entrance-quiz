a2d17d2fcf3f09676af7cd24ed4503666333cb2f
Hourann <tdkgn@outlook.com>
2020-10-27 21:32:43 +0800

    [hourann] refactor: remove unused StudentCell

--- src/App/StudentCell/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import './index.css';
-
-const StudentCell = ({ name, id }) => <div>{`${id}.${name}`}</div>;
-export default StudentCell;
--- src/App/StudentList/index.js
+++ src/App/StudentList/index.js
@@ -1,28 +1,81 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { Component } from 'react';
 import Axios from 'axios';
-import StudentCell from '../StudentCell';
+import './index.css';
 
 export default class StudentList extends Component {
   constructor() {
     super();
     this.state = {
       students: [],
+      isEditing: false,
+      newStudentName: '',
     };
   }
 
-  componentDidMount = () => {
+  fetchStudents = () => {
     Axios.get('http://localhost:8080/students')
       .then((res) => res.data)
       .then((students) => this.setState({ students }));
   };
 
+  componentDidMount = () => {
+    this.fetchStudents();
+  };
+
+  addStudent = (key) => {
+    if (key.which !== 13) return;
+    Axios.post('http://localhost:8080/students', {
+      name: this.state.newStudentName,
+    })
+      .then(() => {
+        this.setState({
+          isEditing: false,
+          newStudentName: '',
+        });
+      })
+      .then(this.fetchStudents);
+  };
+
+  renderAddStudentCell = () => {
+    if (!this.state.isEditing) {
+      return (
+        <div
+          className="student-cell add-student"
+          onClick={() => this.setState({ isEditing: true })}
+        >
+          +添加学员
+        </div>
+      );
+    }
+    return (
+      <input
+        type="text"
+        className="student-cell add-student"
+        value={this.state.newStudentName}
+        onKeyPress={this.addStudent}
+        onChange={({ target }) => this.setState({ newStudentName: target.value })}
+      />
+    );
+  };
+
   render() {
     return (
-      <div>
-        {this.state.students.map(({ id, name }) => (
-          <StudentCell id={id} name={name} />
-        ))}
-      </div>
+      <section>
+        <h1>学员列表</h1>
+        <div className="student-list">
+          {this.state.students.map(({ id, name }) => (
+            <div key={id} className="student-cell">
+              {`${id}.${name}`}
+            </div>
+          ))}
+          {this.renderAddStudentCell()}
+          {/* <button type="button" className="student-cell add-student" onClick={this.addStudent}>
+            +添加学员
+          </button> */}
+        </div>
+      </section>
     );
   }
 }
